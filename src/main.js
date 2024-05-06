38c21c97cebc3ea0815c63ae45574b3d3631a049
thorcazo <thorcazodesign@gmail.com>
2024-05-06 20:02:50 +0200

    Juego basico para probar fisicas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { Boot } from './scenes/Boot';
 import { Game } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
 import { Preloader } from './scenes/Preloader';
 
@@ -8,10 +7,17 @@ import { Preloader } from './scenes/Preloader';
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config = {
     type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
+    width: window.innerWidth,
+    height: window.innerHeight,
     parent: 'game-container',
-    backgroundColor: '#028af8',
+    backgroundColor: '#056',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: true
+        },
+    },
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
@@ -21,7 +27,7 @@ const config = {
         Preloader,
         MainMenu,
         Game,
-        GameOver
+
     ]
 };
 
--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,28 +1,61 @@
 import { Scene } from 'phaser';
 
-export class MainMenu extends Scene
-{
-    constructor ()
-    {
-        super('MainMenu');
+export class MainMenu extends Scene {
+  balls = null; // Inicializa balls como null
+  enemy = Phaser.Physics.Arcade.Image;
+
+  constructor() {
+    super('MainMenu');
+  }
+
+  create() {
+    this.player = this.physics.add.image(200, window.innerHeight / 2, 'player');
+    this.enemy = this.physics.add.image(window.innerWidth - 80, window.innerHeight / 2, 'enemy');
+    this.enemy.setImmovable(true);
+
+    // Crear un grupo de bolas
+    this.balls = this.physics.add.group();
+
+    /* logica cuando pulso ESPACE */
+    this.input.keyboard.on('keydown-SPACE', () => {
+      /* funcion para que ball se desplace a la izquieda */
+      const ball = this.physics.add.image(this.player.x, this.player.y, 'ball');
+      this.balls.add(ball); // Añadir la bola al grupo
+      this.desplazarBall(ball);
+    });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update(time, delta) {
+    /* añadir movimiento a "player" -> "up" y "down" */
+    if (this.cursors.down.isDown) {
+      this.player.y += 5;
+    } else if (this.cursors.up.isDown) {
+      this.player.y -= 5;
     }
 
-    create ()
-    {
-        this.add.image(512, 384, 'background');
-
-        this.add.image(512, 300, 'logo');
-
-        this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
-
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('Game');
-
-        });
+    // Si la bola está fuera de la vista del mundo, hazla invisible
+    if (this.balls) { // Asegúrate de que balls se ha inicializado
+      this.balls.getChildren().forEach(ball => {
+        if (!this.cameras.main.worldView.contains(ball.x, ball.y)) {
+          ball.visible = false;
+        }
+      });
     }
+
+    // Si la bola colisiona con el enemigo, hazla invisible
+    this.physics.collide(this.balls, this.enemy, (ball) => {
+      ball.destroy();
+      this.enemy.destroy(); 
+    });
+  }
+
+  /* funciones del juego */
+  desplazarBall(ball) {
+    /* funcion para que ball se desplace a la izquieda */
+    if (ball) {
+      ball.setVelocityX(+1000);
+    }
+  }
 }
