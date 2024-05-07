e421b2225362ec0da21037f20d50ab39e06b7291
thorcazo <thorcazodesign@gmail.com>
2024-05-07 17:05:38 +0200

    Añadido word para asignar palabra a cada zombie

--- src/objects/Zombie.ts
+++ src/objects/Zombie.ts
@@ -2,10 +2,18 @@ import Phaser, { Physics } from 'phaser'
 
 export default class Zombie extends Phaser.GameObjects.Sprite implements IZombie, Physics.Arcade.Components.Velocity {
   private target?: Phaser.GameObjects.Components.Transform
+  private word: string
+  private wordText: Phaser.GameObjects.Text;
 
-  constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
+  words: string[] = ['apple', 'banana', 'cherry', 'grape', 'lemon', 'melon', 'orange', 'peach', 'pear', 'plum'];
+
+  
+  
+  constructor(scene: Phaser.Scene, x: number, y: number, texture: string, word: string) {
     super(scene, x, y, texture);
     this.scene.physics.world.enable(this) // Habilitar física para el zombie
+    this.word = this.setWord();
+    this.wordText = this.scene.add.text(0, 0, this.word, { fontSize: '16px', color: '#000' });
   }
 
   setTarget(target: Phaser.GameObjects.Components.Transform) {
@@ -25,7 +33,24 @@ export default class Zombie extends Phaser.GameObjects.Sprite implements IZombie
 
     const rotation = Phaser.Math.Angle.Between(x, y, tx, ty)
     this.setRotation(rotation);
-    this.scene.physics.moveToObject(this, this.target, 50) // 100 es la velocidad
+    this.scene.physics.moveToObject(this, this.target, 50) // 50 es la velocidad
+
+    this.wordText.x = this.x;
+    this.wordText.y = this.y + this.height / 2;
 
   }
+  /* metodo para asignar una palabra rando de words */
+  setWord() {
+    return this.word = this.words[Math.floor(Math.random() * this.words.length)];
+  }
+
+
+  /* metodo para destruir wordText */
+  destroyWordText() {
+    if (this.wordText)
+      this.wordText.destroy();
+  }
+
+
+
 }
