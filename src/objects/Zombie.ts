e6daeaf8f55d39e569aba5c6fb253dad3ccaa76d
thorcazo <thorcazodesign@gmail.com>
2024-05-07 12:11:00 +0200

    Añadidos balas para matare zombies

--- src/objects/Zombie.ts
+++ src/objects/Zombie.ts
@@ -1,10 +1,11 @@
-import Phaser from 'phaser'
+import Phaser, { Physics } from 'phaser'
 
-export default class Zombie extends Phaser.GameObjects.Sprite implements IZombie {
+export default class Zombie extends Phaser.GameObjects.Sprite implements IZombie, Physics.Arcade.Components.Velocity {
   private target?: Phaser.GameObjects.Components.Transform
 
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
-    super(scene, x, y, texture)
+    super(scene, x, y, texture);
+    this.scene.physics.world.enable(this) // Habilitar física para el zombie
   }
 
   setTarget(target: Phaser.GameObjects.Components.Transform) {
@@ -23,6 +24,8 @@ export default class Zombie extends Phaser.GameObjects.Sprite implements IZombie
     const y = this.y
 
     const rotation = Phaser.Math.Angle.Between(x, y, tx, ty)
-    this.setRotation(rotation)
+    this.setRotation(rotation);
+    this.scene.physics.moveToObject(this, this.target, 50) // 100 es la velocidad
+
   }
 }
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -4,6 +4,7 @@ import Zombie from '../objects/Zombie';
 export class Game extends Scene {
   balls = null; // Inicializa balls como null
   zombies = null;
+  bullets = null;
 
   constructor() {
     super('Game');
@@ -21,7 +22,12 @@ export class Game extends Scene {
     this.player = this.physics.add.image(200, window.innerHeight / 2, 'player');
 
     // Crear un grupo de bolas
-    this.balls = this.physics.add.group();
+    this.bullets = this.physics.add.group();
+
+    // Crear una tecla de disparo
+    this.shootKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+
     this.cursors = this.input.keyboard.createCursorKeys();
 
     /* Añadir zombies a el grupo zombies */
@@ -48,8 +54,20 @@ export class Game extends Scene {
       zombie.setTarget(this.player)
     })
 
+    // Habilitar la colisión entre los zombies
+    this.physics.add.collider(this.zombies)
+
+    // Habilitar la colisión entre el jugador y los zombies
+    this.physics.add.overlap(this.player, this.zombies, () => {
+      this.scene.pause('Game');
+      this.scene.launch('GameOver');
+    })
+
   } // FINISH CREATE
 
+
+  /* UPDATE
+  ========================== */
   update(time, delta) {
     /* añadir movimiento a "player" -> "up" y "down" */
     if (this.cursors.down.isDown) {
@@ -62,14 +80,58 @@ export class Game extends Scene {
       this.player.x += 5;
     }
 
-    // Si la bola está fuera de la vista del mundo, hazla invisible
-    if (this.balls) { // Asegúrate de que balls se ha inicializado
-      this.balls.getChildren().forEach(ball => {
-        if (!this.cameras.main.worldView.contains(ball.x, ball.y)) {
-          ball.visible = false;
-        }
-      });
+
+    // Disparar una bala
+    if (Phaser.Input.Keyboard.JustDown(this.shootKey)) {
+      const bullet = this.bullets.get(this.player.x, this.player.y, 'ball');
+      if (bullet) {
+        bullet.setActive(true);
+        bullet.setVisible(true);
+        bullet.setRotation(this.player.rotation);
+        this.physics.velocityFromRotation(this.player.rotation, 600, bullet.body.velocity);
+      }
     }
+
+
+    // Añadir una colisión entre las balas y los zombies
+    this.physics.add.collider(this.bullets, this.zombies, (bullet, zombie) => {
+      bullet.destroy(); // Destruir la bala
+      zombie.destroy(); // Destruir el zombie
+
+
+
+      // Crear un nuevo zombie en una posición aleatoria
+      const newZombie = this.zombies.get(
+        Phaser.Math.Between(100, 700),
+        Phaser.Math.Between(100, 500),
+        'zombie'
+      );
+      // Asegurarse de que el nuevo zombie está activo y visible
+      if (newZombie) {
+        newZombie.setActive(true);
+        newZombie.setVisible(true);
+        newZombie.setTarget(this.player);
+      }
+    }, null, this);
+
+    // Hacer que el jugador mire al zombie más cercano
+    let closestZombie = null;
+    let minDist = Infinity;
+    this.zombies.getChildren().forEach(zombie => {
+      const dist = Phaser.Math.Distance.Between(this.player.x, this.player.y, zombie.x, zombie.y);
+      if (dist < minDist) {
+        closestZombie = zombie;
+        minDist = dist;
+      }
+    });
+
+    if (closestZombie) {
+      const angle = Phaser.Math.Angle.Between(this.player.x, this.player.y, closestZombie.x, closestZombie.y);
+      this.player.setRotation(angle);
+    }
+
+
+
   }
 
 }
